30c7bbec0fa1de788ce05e61b93af9d7f28fb535
Seb SS <salinasmaria926@gmail.com>
2018-11-24 23:11:49 +0100

    Made a Felt superclass with a constructor, getters and setters

---
+++ src/main/java/Model/Felt.java
@@ -0,0 +1,49 @@
+package Model;
+
+/**
+ * -------------------------------------------------
+ * Denne superklasse bruges til at gøre det nemmere
+ * at give alle felterne et navn, beksrivelse og
+ * hvilken effekt feltet skal gøre
+ *-------------------------------------------------
+ */
+
+public class Felt {
+    private String navn, subText, beskrivelse;
+
+    //###############Constructor##################
+    public Felt(String navn, String subText, String beskrivelse){
+        this.navn=navn;
+        this.subText=subText;
+        this.beskrivelse=beskrivelse;
+    }
+    //###################Get######################
+    public String Felte(){
+        String help="help";
+        return help;
+    }
+    //#################Get/Set####################
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getSubText() {
+        return subText;
+    }
+
+    public void setSubText(String subText) {
+        this.subText = subText;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+}
