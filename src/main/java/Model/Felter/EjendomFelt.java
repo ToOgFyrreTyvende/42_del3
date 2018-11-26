71c292fdb187a6fc720126528a319feaa8aeda00
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-26 16:59:39 +0100

    Changed vars to private

--- src/main/java/Model/Felter/EjendomFelt.java
+++ src/main/java/Model/Felter/EjendomFelt.java
@@ -12,13 +12,62 @@ import java.awt.*;
 
 public class EjendomFelt extends Felt {
     // Variables
-    int pris;
-    int leje;
-    Spiller ejer;
-    Color farve;
+    private int pris;
+    private int leje;
+    private Spiller ejer;
+    private Color farve;
 
     // #----------Constructor----------#
     public EjendomFelt(String navn, String subText, String beskrivelse) {
         super(navn, subText, beskrivelse);
+    }
+
+    // #--------------Get--------------#
+    @Override
+    public String getNavn() {
+        return super.getNavn();
+    }
+
+    @Override
+    public String getSubText() {
+        return super.getSubText();
+    }
+
+    @Override
+    public String getBeskrivelse() {
+        return super.getBeskrivelse();
+    }
+
+    public int getPris() {
+        return pris;
+    }
+
+    public int getLeje() {
+        return leje;
+    }
+
+    public Spiller getEjer() {
+        return ejer;
+    }
+
+    public Color getFarve() {
+        return farve;
+    }
+
+    // #--------------Get--------------#
+    public void setPris(int pris) {
+        this.pris = pris;
+    }
+
+    public void setLeje(int leje) {
+        this.leje = leje;
+    }
+
+    public void setEjer(Spiller ejer) {
+        this.ejer = ejer;
+    }
+
+    public void setFarve(Color farve) {
+        this.farve = farve;
     }
 }
