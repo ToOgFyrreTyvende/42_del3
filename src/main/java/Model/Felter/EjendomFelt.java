7bc4807c4e57a071a93b12a2e746ea0851aa4886
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-26 16:50:46 +0100

    Created EjendomFelt

--- src/main/java/Model/Felter/EjendomFelt.java
+++ src/main/java/Model/Felter/EjendomFelt.java
@@ -1,4 +1,24 @@
 package Model.Felter;
 
-public class EjendomFelt {
+import Model.Felt;
+import Model.Spiller;
+
+import java.awt.*;
+
+/**
+ * @author Xephyz
+ * Denne klasse holder styr på alle felter der handler om Ejendomme som kan købes af spillere
+ */
+
+public class EjendomFelt extends Felt {
+    // Variables
+    int pris;
+    int leje;
+    Spiller ejer;
+    Color farve;
+
+    // #----------Constructor----------#
+    public EjendomFelt(String navn, String subText, String beskrivelse) {
+        super(navn, subText, beskrivelse);
+    }
 }
