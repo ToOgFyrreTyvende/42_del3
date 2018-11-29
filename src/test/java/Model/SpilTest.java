17ea1a82c22fcae527640e7eb5fc9bef8d389c0c
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-29 12:25:11 +0100

    Replaced spilTest with autogenerated version
    Deleted spilTest.java to create the test with IntelliJ which auto-generates empty tests.

---
+++ src/test/java/Model/SpilTest.java
@@ -0,0 +1,89 @@
+package Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void spilTur() {
+    }
+
+    @Test
+    void checkRunde() {
+    }
+
+    @Test
+    void findVinder() {
+    }
+
+    @Test
+    void getSpillere() {
+    }
+
+    @Test
+    void setSpillere() {
+    }
+
+    @Test
+    void getVinder() {
+    }
+
+    @Test
+    void setVinder() {
+    }
+
+    @Test
+    void getAktivSpiller() {
+    }
+
+    @Test
+    void setAktivSpiller() {
+    }
+
+    @Test
+    void getTerning() {
+    }
+
+    @Test
+    void setTerning() {
+    }
+
+    @Test
+    void getRunder() {
+    }
+
+    @Test
+    void setRunder() {
+    }
+
+    @Test
+    void getAktivRunde() {
+    }
+
+    @Test
+    void setAktivRunde() {
+    }
+
+    @Test
+    void isAfsluttet() {
+    }
+
+    @Test
+    void setAfsluttet() {
+    }
+
+    @Test
+    void getStartPenge() {
+    }
+
+    @Test
+    void setStartPenge() {
+    }
+}
