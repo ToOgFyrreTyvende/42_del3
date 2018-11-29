d0405aaad5849c838ce29fd50f6ce71c696d7007
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-29 12:55:43 +0100

    Update SpilTest.java
    WIP!!! Doesn't work yet!!!

--- src/test/java/Model/SpilTest.java
+++ src/test/java/Model/SpilTest.java
@@ -6,84 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpilTest {
+    // Variables
+    Spil testSpil;
+    String[] testNavne;
+    Spiller[] testSpillere;
 
     @BeforeEach
     void setUp() {
+        testNavne = new String[2];
+        testNavne[0] = "dummyOne";
+        testNavne[1] = "dummyTwo";
+
+        testSpillere[0] = new Spiller(testNavne[0]);
+        testSpillere[1] = new Spiller(testNavne[1]);
+
+
+
+        testSpil = new Spil(testNavne);
     }
 
     @Test
-    void spilTur() {
-    }
-
-    @Test
-    void checkRunde() {
-    }
-
-    @Test
-    void findVinder() {
-    }
-
-    @Test
-    void getSpillere() {
-    }
-
-    @Test
-    void setSpillere() {
-    }
-
-    @Test
-    void getVinder() {
-    }
-
-    @Test
-    void setVinder() {
-    }
-
-    @Test
-    void getAktivSpiller() {
-    }
-
-    @Test
-    void setAktivSpiller() {
-    }
-
-    @Test
-    void getTerning() {
-    }
-
-    @Test
-    void setTerning() {
-    }
-
-    @Test
-    void getRunder() {
-    }
-
-    @Test
-    void setRunder() {
-    }
-
-    @Test
-    void getAktivRunde() {
-    }
-
-    @Test
-    void setAktivRunde() {
-    }
-
-    @Test
-    void isAfsluttet() {
-    }
-
-    @Test
-    void setAfsluttet() {
-    }
-
-    @Test
-    void getStartPenge() {
-    }
-
-    @Test
-    void setStartPenge() {
+    void SpillereTest() {
+        testSpil.setSpillere(testSpillere);
+        assertTrue(testSpil.getSpillere() == testSpillere);
     }
 }
