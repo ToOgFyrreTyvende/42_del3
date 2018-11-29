d2ebffdeb62fc28bc894588366de40d0aaf24d40
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-29 12:25:32 +0100

    Create SpilControllerTest.java
    Created test file for SpilController

---
+++ src/test/java/Controller/SpilControllerTest.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import Model.Spil;
+import Model.Spiller;
+import View.GameView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpilControllerTest {
+
+    @BeforeEach
+    void setUp() {
+//        String[] testSpillerNavne = new String[2];
+//        testSpillerNavne[0] = "dummyOne";
+//        testSpillerNavne[1] = "dummyTwo";
+//
+//        Spil testSpil = new Spil(testSpillerNavne);
+//        GameView testView;
+//        SpilController testSpilCtrl = new SpilController();
+
+
+    }
+
+    @Test
+    void spillerTur() {
+    }
+
+    @Test
+    void spilAktivt() {
+    }
+}
