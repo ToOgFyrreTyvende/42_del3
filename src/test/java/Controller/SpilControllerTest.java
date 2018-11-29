c09171aaa71fad02f36eea955a3947f88eead6de
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-29 12:55:29 +0100

    Update SpilControllerTest.java
    WIP! Not finished!

--- src/test/java/Controller/SpilControllerTest.java
+++ src/test/java/Controller/SpilControllerTest.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.Spil;
 import Model.Spiller;
+import View.GameGUIView;
 import View.GameView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,25 +10,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpilControllerTest {
+    // Variables
+    Spil testSpil;
+    GameView testView;
+    SpilController testSpilCtrl;
+
 
     @BeforeEach
     void setUp() {
-//        String[] testSpillerNavne = new String[2];
-//        testSpillerNavne[0] = "dummyOne";
-//        testSpillerNavne[1] = "dummyTwo";
-//
-//        Spil testSpil = new Spil(testSpillerNavne);
-//        GameView testView;
-//        SpilController testSpilCtrl = new SpilController();
+        String[] testSpillerNavne = new String[4];
+        testSpillerNavne[0] = "dummyOne";
+        testSpillerNavne[1] = "dummyTwo";
+        testSpillerNavne[2] = "dummyThree";
+        testSpillerNavne[3] = "dummyFour";
 
+        testSpil = new Spil(testSpillerNavne);
+        testView = new GameGUIView(); // TODO: Lav en stub klasse for at undgå tests der åbner et GUI
+        testSpilCtrl = new SpilController();
 
     }
 
     @Test
-    void spillerTur() {
+    void spillerTurTest() {
     }
 
     @Test
-    void spilAktivt() {
+    void spilAktivtTest() { // Fjern // efter stubklassen er blevet lavet
+        //assertTrue(!testSpil.isAfsluttet());
     }
 }
