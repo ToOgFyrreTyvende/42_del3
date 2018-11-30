1c6ccd2583fe048b7ef2071bf846c5729d53774b
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-30 08:40:08 +0100

    Update TerningTest.java
    Completed (maybe... IntelliJ disagrees for some reason) test for Terning

--- src/test/java/TerningTest.java
+++ src/test/java/TerningTest.java
@@ -1,15 +1,21 @@
 import Model.Terning;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TerningTest {
-    Terning terning = new Terning();
+    Terning testTerning1;
+
+    @BeforeEach
+    void setUp() {
+        testTerning1 = new Terning();
+    }
 
     @Test
-    void getResultat() {
-       // Ingen normalfordeling :(
+    void getResultatTest() {
+       int testRes = testTerning1.getResultat();
+       // Tester at terningen kun giver værdier fra 1 til 6
+       assertTrue(testRes > 0 && testRes < 7);
     }
 }
