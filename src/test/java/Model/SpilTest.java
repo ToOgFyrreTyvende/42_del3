04a448f6bbdd10283e99b5c9858f6507fb1cc7b3
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-30 10:32:55 +0100

    Update SpilTest.java
    More tests for Spil class
    - spilTurTest
    - spilReglerFaengselTest
    - spilReglerStartTest
    
    Co-Authored-By: flaremew <flaremew@users.noreply.github.com>
    Co-Authored-By: isling98 <isling98@users.noreply.github.com>
    Co-Authored-By: lysberg <lysberg@users.noreply.github.com>
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>
    Co-Authored-By: Ahmad Sattar <thehabbos007@users.noreply.github.com>
    Co-Authored-By: Anders Michaelsen <xephyz@users.noreply.github.com>

--- src/test/java/Model/SpilTest.java
+++ src/test/java/Model/SpilTest.java
@@ -2,6 +2,7 @@ package Model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stub.TerningStub;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,8 @@ class SpilTest {
     Spil testSpil;
     String[] testNavne;
     Spiller[] testSpillere;
+    GameBoard testBoard;
+    TerningStub testTerning;
 
     @BeforeEach
     void setUp() {
@@ -17,17 +20,58 @@ class SpilTest {
         testNavne[0] = "dummyOne";
         testNavne[1] = "dummyTwo";
 
-        testSpillere[0] = new Spiller(testNavne[0]);
-        testSpillere[1] = new Spiller(testNavne[1]);
+        testBoard = new GameBoard();
 
+        testTerning = new TerningStub();
 
-
-        testSpil = new Spil(testNavne);
+        testSpil = new Spil(testBoard, testNavne);
     }
 
     @Test
     void SpillereTest() {
         testSpil.setSpillere(testSpillere);
         assertTrue(testSpil.getSpillere() == testSpillere);
+    }
+
+    @Test
+    void spilTurTest() {
+        testSpil.setAfsluttet(false);
+        testSpil.spilTur();
+        assertTrue(testSpil.getAktivSpiller() == testSpil.getSpillere()[1]);
+
+        testSpil.setAfsluttet(true);
+        assertTrue(testSpil.spilTur() == null);
+    }
+
+    @Test
+    void spilReglerFaengselTest() {
+        // Sætter aktiv spiller i fængsel
+        testSpil.getAktivSpiller().setiFaengsel(true);
+        // Sætter aktiv spiller på fængsel feltet
+        testSpil.getAktivSpiller().setFelt(testBoard.getFaengsel());
+        // Sætter aktiv spiller's pengeværdi til 42
+        // - (For at undgå spilleren går falit og afslutter spillet)
+        testSpil.getAktivSpiller().setPenge(42);
+        // Kører spilRegler metoden
+        testSpil.spilRegler(testSpil.getAktivSpiller().getFelt());
+        // Tjekker at aktiv spiller ikke længere er i fængsel efter metodekaldet
+        assertTrue(!testSpil.getAktivSpiller().isiFaengsel());
+        // Tjekker at aktiv spiller har fået trukket 1 fra spillerens pengeværdi (ud af fængsel pris)
+        assertTrue(testSpil.getAktivSpiller().getPenge() == 41);
+    }
+
+    @Test
+    void spilReglerStartTest() {
+        // Sætter spillet til at bruge vores TerningStub som kun ruller værdien 1
+        testSpil.setTerning(testTerning);
+        int testVal = 42;
+        // Sætter spiller 1's pengeværdi til 42
+        testSpil.getSpillere()[0].setPenge(testVal);
+        // Sætter spiller 1 på brættets sidste felt (23)
+        testSpil.getSpillere()[0].setFelt(23);
+        // Kører spiller 1's tur færdig
+        testSpil.spilTur();
+        // Tjækker spiller 1's pengeværdi er steget med 2
+        assertTrue(testSpil.getSpillere()[0].getPenge() == testVal + testSpil.getRUNDE_PENGE());
     }
 }
