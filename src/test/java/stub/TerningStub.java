b3d0dbac521f307f4fcb9ab46eecc76b51bbd489
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-30 10:33:42 +0100

    Update TerningStub.java
    TerningStub now extends Terning class, and only returns a value of 1
    
    Co-Authored-By: flaremew <flaremew@users.noreply.github.com>
    Co-Authored-By: isling98 <isling98@users.noreply.github.com>
    Co-Authored-By: lysberg <lysberg@users.noreply.github.com>
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>
    Co-Authored-By: Ahmad Sattar <thehabbos007@users.noreply.github.com>
    Co-Authored-By: Anders Michaelsen <xephyz@users.noreply.github.com>

--- src/test/java/stub/TerningStub.java
+++ src/test/java/stub/TerningStub.java
@@ -1,11 +1,15 @@
 package stub;
 
+import Model.Terning;
+
 /**
  * terningStub
  */
-public class TerningStub {
+public class TerningStub extends Terning {
 
-    public int[] getResultat() {
-        return new int[]{6,6,10};
+    @Override
+    public int getResultat() {
+        int testRes = 1;
+        return testRes;
     }
 }
